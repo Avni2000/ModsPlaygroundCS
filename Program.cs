de16f862f63075feb4dfb5042069202890d943fb
Avni2000 <avnibadiwale@gmail.com>
2025-04-22 02:12:59 -0500

    yeah i still don't know what's up lol. Is this what good practice looks like?

--- Program.cs
+++ Program.cs
@@ -3,39 +3,89 @@
 using System.Collections;
 using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
 
-namespace Program
+namespace ModsPlaygroundCS
 {
-    public class recurse(int n)
+    public class Recurse(int n)
     {
         private int maxResidue = n;
 
         public static void Main(string[] args)
         {
-            /*suppose you have the typical {P_EPT_IDE_} where "_"represents a residue, Max residues are given as n. 
+            /*
+            suppose you have the typical {P_EPT_IDE_} where "_"represents a residue, Max residues are given as n.
             and you want to find the cartesian product/powerset of the residues up till a certain length, defined as m.
             Now let's consider the residues as a set of numbers, weighted individually. Suppose 1-3 residues are weighted 2, 2, 4 like in the example
-             */
-            int n = 3; //max residue
+            */
+            int n = 3; //max residues
             int m = 2; //max length
             List<int> toPowerList = new List<int>();
             toPowerList.Add(2);
             toPowerList.Add(2);
             toPowerList.Add(4);
-            WeightedPowerset(2, toPowerList, new List<List<int>>());
+                 //   var res = WeightedPowerset(2, toPowerList, new List<List<int>>());
+             //    for(int i = 0; i< res.Count; i++)
+            //     {
+          //      Console.WriteLine("Set: " + i + " = " + string.Join(", ", res[i]));
+            //      }
+         var newList= WeightedPowerset(2, new List<List<int>>(), new List<int>(), toPowerList);
+         Console.WriteLine(newList.Count);
+         
+      //   for (int i = 0; i < newList.Count; i++)
+       //  {
+      //       Console.WriteLine(newList[i]);
+      //   }
         }
 
+        public static List<List<int>> WeightedPowerset(int m, List<List<int>> result, List<int> subset, List<int> OG)
+        {
+            if (subset.Count <= m && !result.Contains(subset)) // collect subset if valid and not already in result
+            {
+                result.Add(new List<int>(subset));
+            }
 
-        public static List<List<int>> WeightedPowerset (int m, List<int> toPowerList, List<List<int>> result) 
+            if (OG.Count == 0)
+            {
+                return result;
+            }
+            int currentSite = OG[0]; //where OG[0] is the current residue. To add or not to add?
+            //case 1: exclude current
+            List<int> remaining = new List<int>(OG);
+            remaining.Remove(currentSite);
+            //recursively call the function w/o current integer.
+            result.AddRange(WeightedPowerset(m, result, subset, remaining)); //essentially removing from toPowerList
+            
+            if(subset.Count+1 >m)
+            {
+                return result;
+            }
+            //case 2: include current
+            //consider if adding current to set keeps length less or equal to m.
+            List<int> include = new List<int>(subset);
+            include.Add(currentSite);
+            if (include.Count <= m) 
+            {
+                result.AddRange(WeightedPowerset(m, result, include, OG));
+            }
+
+            return result;
+        }
+
+        /*
+
+
+        public static List<List<int>> WeightedPowerset(int m, List<int> toPowerList, List<List<int>> result)
         {
             //quick note: List.addAll = List.addRange.
-            
+
             //we start with the empty set result, toPowerList should be full before recursive cases.
             if (toPowerList.Count == 0)
             {
                 return result;
             }
+
             int current = toPowerList[0];
             //case 1: exclude current
             List<int> remaining = new List<int>(toPowerList);
@@ -50,7 +100,7 @@ namespace Program
             include.Add(current);
             if (include.Count <= m) //note that the only way to add to empty set 
             {
-                result.AddRange(WeightedPowerset(m, toPowerList,result));
+                result.AddRange(WeightedPowerset(m, include, result));
 
             }
             else
@@ -60,18 +110,17 @@ namespace Program
                 result.AddRange(WeightedPowerset(m, include, result));
             }
 
-            return result; 
+            return result;
         }
-
-        //can we modify the above slightly to output a list of integers instead? How about just a single integer? 
+        /*
+        //can we modify the above slightly to output a list of integers instead? How about just a single integer?
         // At what point does it integer overflow, and is it worthwhile to actually represent that? I don't believe so. .
         public static int cartesianProd(int m, List<int> toPowerList, int result)
         {
-            
+
             /*
              *We don't need any other changes than those done to return methods.
-             *
-             */
+
 
 
             //we start with the empty set result, toPowerList should be full before recursive cases.
@@ -86,12 +135,12 @@ namespace Program
             //recursively call the function w/o current integer.
             var exclude = (WeightedPowerset(m, remaining, result)); //essentially removing from toPowerList
             //exclude contains all possible sets wherein current is NOT included.
-            
+
             //case 2: include current
             //consider if adding current to set keeps length less or equal to m.
             List<int> include = new List<int>(toPowerList);
             include.Add(current);
-            if (include.Count <= m) //note that the only way to add to empty set 
+            if (include.Count <= m) //note that the only way to add to empty set
             {
                 include = (WeightedPowerset(m, toPowerList, include));
 
@@ -103,15 +152,15 @@ namespace Program
                 int other =  (WeightedPowerset(m, include, ));
             }
 
-            return 
+            return
 
 
-        }
+
 
         public static int PI(List<int> x)
         {
             int product = 1;
-            //helper method to output a single integer given a list. 
+            //helper method to output a single integer given a list.
             for (int i = 0; i < x.Count; i++)
             {
                 //suppose no numbers are 0.
@@ -119,6 +168,7 @@ namespace Program
             }
 
             return product;
-        } 
+        }
+    */
     }
 }
